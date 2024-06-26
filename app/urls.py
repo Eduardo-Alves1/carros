4b1b5ec778bccc1ffae6ce94b082578225624cd0
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-06-26 18:08:08 -0300

    feat

--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_viwe, new_car_view
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', cars_viwe, name='cars_list'),
     path('new_car/',new_car_view, name='new_car'),
+    path('registers/',register_view, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #CONFIGURAÇÃO DE IMAGENS
 
 
