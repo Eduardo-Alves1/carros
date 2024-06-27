f7d785653aebe98fa14a74ea5b7a351a161c8cd5
Eduardo Alves <41787124+Eduardo-Alves1@users.noreply.github.com>
2024-06-27 14:32:21 -0300

    Sitema De Login

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_viwe, new_car_view
-from accounts.views import register_view
+from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', cars_viwe, name='cars_list'),
     path('new_car/',new_car_view, name='new_car'),
     path('registers/',register_view, name='register'),
+    path('login/',login_view, name='login'),
+    path('logout/',logout_view, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #CONFIGURAÇÃO DE IMAGENS
 
 
