d68808c69abbe641114ac5f37b0b0536ce193870
Eduardo Alves <eduardo.santos.alves14@gmail.com>
2024-08-21 18:39:01 -0300

    Updade ordering Brands

--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,9 @@ class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
    
